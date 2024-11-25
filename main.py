87a6a023d74df75b39ee9da713cc835358c84dbd
ThePrimeagen <carlosberti182@gmail.com>
2024-11-25 14:07:08 -0300

    create circleshape class and first initial player class

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
        screen.fill("black")
+
+       player.draw(screen)
+
        pygame.display.flip()
        time_since_last_time = clock.tick(60)
        dt = time_since_last_time / 1000
